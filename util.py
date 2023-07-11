c79d5d19346400f11ca31b018fe8e13939c054da
ariqbasyar <ariqbasyar2@gmail.com>
2023-07-11 17:18:14 +0700

    improve get size function

--- util.py
+++ util.py
@@ -1,9 +1,10 @@
+import base64
+import cv2
+import inspect
+import numpy as np
 import os
 import sys
-import base64
 import uuid
-import cv2
-import numpy as np
 
 from datetime import datetime
 from io import BytesIO
@@ -20,8 +21,44 @@ def numpy_to_bytes(arr):
     np.save(f,arr)
     return f.getvalue()
 
-def size_kb(s, decimals=2):
-    _size = sys.getsizeof(s)
+def get_size(obj, seen=None):
+    """Recursively finds size of objects in bytes.\n
+    Modified version of
+    https://github.com/bosswissam/pysize/blob/master/pysize.py"""
+    size = sys.getsizeof(obj)
+    if seen is None:
+        seen = set()
+    obj_id = id(obj)
+    if obj_id in seen:
+        return 0
+    # Important mark as seen *before* entering recursion to gracefully handle
+    # self-referential objects
+    seen.add(obj_id)
+    if hasattr(obj, '__dict__'):
+        for cls in obj.__class__.__mro__:
+            if '__dict__' in cls.__dict__:
+                d = cls.__dict__['__dict__']
+                if inspect.isgetsetdescriptor(d) \
+                    or inspect.ismemberdescriptor(d):
+                    size += get_size(obj.__dict__, seen)
+                break
+    if isinstance(obj, dict):
+        size += sum((get_size(v, seen) for v in obj.values()))
+        size += sum((get_size(k, seen) for k in obj.keys()))
+    elif hasattr(obj, '__iter__') and not isinstance(obj,
+                                                     (str, bytes, bytearray)):
+        try:
+            size += sum((get_size(i, seen) for i in obj))
+        except TypeError:
+            pass
+    if hasattr(obj, '__slots__'): # can have __slots__ with __dict__
+        size += sum(get_size(getattr(obj, s), seen)\
+                    for s in obj.__slots__ if hasattr(obj, s))
+
+    return size
+
+def size_kb(obj, decimals=3):
+    _size = get_size(obj)
     return round(_size/1e3, decimals)
 
 DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S.%f'
