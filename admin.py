4e673251077d125155d6348df1d419fdf6b617f7
ariqbasyar <ariqbasyar2@gmail.com>
2023-12-20 16:56:34 +0700

    preparing admin.py to make it import-able

--- admin.py
+++ admin.py
@@ -10,6 +10,8 @@ def check_server_env(server):
     for server_name, host in new.items():
         if host.startswith('$'):
             host = os.getenv(host[1:])
+            assert host is not None,\
+                f'Host {host} couldn\'t be found in env variables.'
         new[server_name] = host
     return new
 
@@ -24,6 +26,7 @@ def create_topic(topics, admin):
             raise exc
 
 def alter_config(topic_name, adm, config):
+    if len(config) == 0: return
     res = adm.alter_configs([
         admin.ConfigResource(admin.ConfigResource.Type.TOPIC,
                             topic_name,
@@ -44,7 +47,7 @@ def create_partitions(topic_name, adm, num_partitions):
             if err.code() == KafkaError.INVALID_PARTITIONS: continue
             raise exc
 
-def main(filepath: Path):
+def read_topic_yaml(filepath: Path, str_format={}):
     admins = {}
     with filepath.open() as f:
         config = yaml.safe_load(f)
@@ -52,32 +55,39 @@ def main(filepath: Path):
     server = check_server_env(config['server'])
     for server_name, host in server.items():
         admins[server_name] = admin.AdminClient({'bootstrap.servers':host})
-    for topic_name, attr in topic.items():
-        _partition = attr['partitions']
-        _config = attr['config']
+    for topic, attr in topic.items():
+        _partition = attr.get('partitions', 1)
+        _config = attr.get('config', {})
         _server = attr['server']
-        _topic = admin.NewTopic(topic_name,
-                                num_partitions=_partition,
-                                config=_config)
+
+        _pattern = attr.get('pattern')
+        if _pattern is not None:
+            _topic_name = _pattern.format(**str_format)
+        else:
+            _topic_name = topic
+        new_topic_instance = admin.NewTopic(_topic_name,
+                                            num_partitions=_partition,
+                                            config=_config)
+
         if not isinstance(_server, list):
             _server = [_server]
         for server_name in _server:
             adm = admins[server_name]
             try:
-                create_topic(_topic, adm)
+                create_topic(new_topic_instance, adm)
                 status = 'created'
             except KafkaException as e:
                 err = e.args[0]
                 if err.code() == KafkaError.TOPIC_ALREADY_EXISTS:
-                    print(f'topic {topic_name} on server {server_name} already \
+                    print(f'Topic {_topic_name} on server {server_name} already \
 exists, altering configs...')
-                    create_partitions(topic_name, adm, _partition)
-                    alter_config(topic_name, adm, _config)
+                    create_partitions(_topic_name, adm, _partition)
+                    alter_config(_topic_name, adm, _config)
                     status = 'altered'
 
-        print(f'topic {topic_name} for server {_server} has {status}')
-    print('done')
+        print(f'Topic {_topic_name} for server {_server} has been {status}')
+    print('Done')
 
 if __name__ == '__main__':
     filepath = Path(sys.argv[1])
-    main(filepath)
+    read_topic_yaml(filepath)
